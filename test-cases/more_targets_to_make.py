1976c0b8c940f33b99e725e384a0acc399a8140a
sftcd <stephen.farrell@cs.tcd.ie>
2024-07-16 14:33:14 +0000

    add explanation

--- test-cases/more_targets_to_make.py
+++ test-cases/more_targets_to_make.py
@@ -1,9 +1,34 @@
 
 from test_cases_settings import *
 
+
+'''
+Each element of the array specifies a test case. The first one will map to the
+DNS name many10-ng.test.defo.ie and accessing the URL below shoulo give info on
+whether ECH happened (if the test is such that things ought work):
+
+            https://many10.test.defo.ie/echstat.php?format=json
+
+The fields that can be used for each test case are:
+
+    - id - short string (don't include any "-") used in DNS label
+    - expected - whether we think this should work (for human consumption, so
+      text is ok here)
+    - description - a string that'll be published in the DNS as a TXT RR (and
+      elsewhere) to help remember what the test does
+    - encoding: presentation syntax for an HTTPS RR, or, an array of such, if
+      we should have more than one
+    - (optional) noaddr: if this has the value 1 (as a number, not string) then
+      we won't publish an A/AAAA to go with the name
+
+The presentation syntax encoding can make use of variables that are set in the
+test_cases_settings file, e.g. good_ipv4 is the IPv4 address of our test VM.
+After printing, the result should be presentation syntax that'll be accepted by
+bind's `nsupdate -l` command.
+'''
 more_targets_to_make=[
     {
-      'id': 'many-10', 'expected': 'success',
+      'id': 'many10', 'expected': 'success',
       'description': '10 values in HTTPS RR',
       'encoding':
         [
