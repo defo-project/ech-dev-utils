d9a071895bebb8712f4a358b1c9bd893b7b9d3f4
Stephen Farrell <stephen.farrell@cs.tcd.ie>
2024-08-01 13:08:33 +0100

    added HTTPS RR for base_domain

--- test-cases/test_cases_gen.py
+++ test-cases/test_cases_gen.py
@@ -516,6 +516,8 @@ def resetdnscommands():
     print("update add " + dodgy + " " + str(ttl) + " HTTPS", '1 . ech=YWJjCg==' , file=outf)
     print("update add " + dodgy + " " + str(ttl) + " HTTPS", '1 . ech' , file=outf)
     print("update add " + dodgy + " " + str(ttl) + " HTTPS", '10000 . ech=dG90YWwtY3JhcAo=' , file=outf)
+    # may as well have ECH work for the base domain
+    print("update add " + base_domain + " " + str(ttl) + " HTTPS", "1 . ech=",good_kp['encoding'], file=outf)
 
 def up_instrs(name, ttl, a, aaaa, desc, https_rr):
     # print commands to delete then, if needed, add the various records
