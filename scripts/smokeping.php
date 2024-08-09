521f441d2541ac3ffcf7a0f68647d3898ea44d4e
sftcd <stephen.farrell@cs.tcd.ie>
2024-08-09 13:16:18 +0000

    smokey

--- scripts/smokeping.php
+++ scripts/smokeping.php
@@ -24,54 +24,83 @@
 <h1>DEfO ECH Smokeping-like Check</h1>
 
 <p>The (server-side) script on this page displays the results from attempting
-ECH (via curl) with each of the URLs shown, on the date shown.</p>
-
-<!--
-<form method="POST">
-<fieldset style="width:60%">
-<legend align="center">DNS name and port (default: 443)</legend>
-    name: <input name="domain2check" type="text" size="20"/>
-    port: <input name="port2check" type="number" size="5" maxlength="5" value="443"/>
-    <input type="submit" value="Submit">
-</fieldset>
-</form>
--->
+ECH (via curl) with each of the URLs shown, for the time of the run shown.
+Currently, runs are done hourly via cron. Note that "success" means that we
+got the expected result, which, for some of these URLs, means we saw the
+expected error.</p>
 
 <?php
-    /*
-     * General functions
-     */
 
-    $topdir="/var/extra/smokeping/runs";
-    $thisone="";
-    $runlist=glob($topdir . '/*' , GLOB_ONLYDIR);
-    echo "<p>";
-    var_dump($runlist);
-    echo "</p><p>";
-    $run=end($runlist);
-    var_dump($run);
-    echo "</p><p>";
-    $runtime=basename($run);
-    var_dump($runtime);
-    echo "</p>";
-    $runtable=file_get_contents($run."/".$runtime.".html");
-    echo $runtable;
-?>
-
-<?php
     //echo "<p>".date(DATE_ATOM)."</p>";
     // Trim array values using this function "trim_value"
     function trim_value(&$value) {
         $value = trim($value);    // remove whitespace and related from beginning and end of the string
     }
-    array_filter($_POST, 'trim_value');    // the data in $_POST is trimmed
+    array_filter($_GET, 'trim_value');    // the data in $_GET is trimmed
     $postfilter =    // set up the filters to be used with the trimmed post array
         array(
             // we are using this in the url
-            'run' => array('filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW),    
+            'runtime' => array('filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW),    
         );
     // must be referenced via a variable which is now an array that takes the place of $_POST[]
-    $rparr = filter_var_array($_POST, $postfilter);    
+    $rparr = filter_var_array($_GET, $postfilter);    
+
+    $topdir="/var/extra/smokeping/runs";
+    $runlist=glob($topdir . '/*' , GLOB_ONLYDIR);
+
+    $runind=0;
+    $numruns=count($runlist);
+    if ($numruns>0) {
+        $runind=$numruns-1;
+    }
+
+    //var_dump($runlist);
+    $first=reset($runlist);
+    //var_dump($first);
+    $last=end($runlist);
+    //var_dump($last);
+
+    $setfromurl=0;
+    if (isset($rparr['runtime'])) {
+	$trt=$topdir."/".$rparr['runtime'];
+	// var_dump($trt);
+	// sanity check
+	if (strlen($trt) == strlen($last)
+	    && (($key=array_search($trt,$runlist,true))!==false)) {
+	    $thisrun=$trt;
+	    //var_dump($thisrun);
+    	    $setfromurl=1;
+	    //var_dump($key);
+	    $runind=$key;
+	}
+    }
+    if ($setfromurl!=1) {
+	$thisrun=$last;
+    }
+    $runtime=basename($thisrun);
+    // present run
+    //var_dump($runtime);
+    $prevstr="<p>prev</p>";
+    if ($runind>0) {
+    	$prevtime=basename($runlist[$runind-1]);
+    	//var_dump($prevtime);
+    	$prevurl="https://test.defo.ie/smokeping.php?runtime=".$prevtime;
+    	$prevstr="<p><a href=\"".$prevurl."\">prev</a></p>";
+    }
+    $nextstr="<p>next</a>";
+    if ($thisrun!=$last) {
+    	$nexttime=basename($runlist[$runind+1]);
+    	//var_dump($nexttime);
+    	$nexturl="https://test.defo.ie/smokeping.php?runtime=".$nexttime;
+    	$nextstr="<p><a href=\"".$nexturl."\">next</a></p>";
+    }
+    echo "<table border=\"0\" style=\"width:80%\"><tr>";
+    echo "<td align=\"left\">$prevstr</td>";
+    echo "<td width=\"50%\"><h2>Run: ".$runtime."</h2></td>";
+    echo "<td align=\"right\">$nextstr</td>";
+    echo "</tr></table>";
+    $runtable=file_get_contents($thisrun."/".$runtime.".html");
+    echo $runtable;
 ?>
 
 <br> This fine domain brought to you by <a href="https://defo.ie/">DEfO.ie</a> 
