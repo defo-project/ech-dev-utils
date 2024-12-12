171a8e77b5a10a355bb6bc5dc05a5c8060e7d3b2
sftcd <stephen.farrell@cs.tcd.ie>
2024-12-12 12:33:54 +0000

    tweak

--- scripts/smokeping-summary-report.py
+++ scripts/smokeping-summary-report.py
@@ -9,12 +9,14 @@ from argparse import ArgumentParser
 from pprint import pprint
 
 def latex_out(measures):
+    # set the threshold for marking results
+    threshold = 0.95
     # make latex output
     urlind = 0
     # header
     tabhead="""\\tiny
 \\begin{longtblr} [
-        caption = {Interop tests from %s to %s},
+        caption = {ECH interop tests from %s to %s.\\\\ When less than %s percent of tests are as expected, the cell is in bold text.},
         label = {tab:itests}
     ] {
         colspec = {| c | l | c | c | c | c | c | c |},
@@ -24,11 +26,14 @@ def latex_out(measures):
     tabtail="""\\hline
 \\end{longtblr}
 \\normalsize"""
-    print(tabhead % (start_date, end_date))
+    print(tabhead % (start_date, end_date, "{:0.0f}".format(threshold*100)))
     cols = ""
     for c in clients:
         cols += f' & {c} '
     print(f'num & url {cols}\\\\ \\hline')
+    # accumulate some numbers
+    tot_exps = { 'chromium': 0, 'curl': 0, 'firefox': 0, 'golang': 0, 'rustls': 0, 'python': 0 }
+    tot_fails = { 'chromium': 0, 'curl': 0, 'firefox': 0, 'golang': 0, 'rustls': 0, 'python': 0 }
     # one line per URL, columns are url-number, url, per-client: %expected/total
     for u in ue_list:
         urlind += 1
@@ -38,17 +43,43 @@ def latex_out(measures):
                 exps = measures[(u,c)][0]
                 fails = measures[(u,c)][1]
                 tot = exps + fails
+                tot_exps[c] += exps
+                tot_fails[c] += fails
                 if tot == 0:
                     cols += f' & 0/0 '
                 else:
                     percent = "{:.2f}".format(exps/tot)
-                    if (exps/tot) < 0.9:
+                    if (exps/tot) < threshold:
                         cols += ' & \\textbf{'+ f'{percent}/{tot} ' + '} '
                     else:
                         cols += f' & {percent}/{tot} '
             else:
                 cols += ' & n/a '
         print("%d & \\url{%s} %s\\\\ \\hline" % (urlind, u, cols))
+    # total line
+    texpstr=''
+    oexps=0
+    otot=0
+    for c in clients:
+        stot = tot_exps[c]+tot_fails[c]
+        otot += stot
+        oexps += tot_exps[c]
+        if stot != 0:
+            percent = "{:.2f}".format(tot_exps[c]/stot)
+            if tot_exps[c]/stot < threshold:
+                texpstr += ' & \\textbf{'+ f'{percent}/{stot} ' + '} '
+            else:
+                texpstr += f' & {percent}/{stot} '
+        else:
+            texpstr += ' & n/a '
+    print(' & Totals ' + texpstr + '\\\\ \\hline')
+
+    percent = "{:.2f}".format(oexps/otot)
+    if oexps/otot < threshold:
+        print(' & Overall: \\textbf{' + f'{percent}/{otot}' + '} \\\\ \\hline')
+    else:
+        print(f' & Overall: {percent}/{otot} \\\\ \\hline')
+
     print(tabtail)
 
 if __name__ == "__main__":
@@ -60,7 +91,6 @@ if __name__ == "__main__":
     parser.add_argument('--dest_dir', default='/var/extra/sp-tabs',
                         help='directory to put resulting summary file')
     parser.add_argument("-v", "--verbose", action="store_true",  help="additional output")
-    parser.add_argument("--hours", type=int, help="only show last N hours")
     parser.add_argument('-s','--start', dest='start', help='start-date')
     parser.add_argument('-e','--end', dest='end', help='end-date')
     args = parser.parse_args()
