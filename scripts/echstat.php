c0672fbbf8e8776472e4edc6e87b9551ed87c747
sftcd <stephen.farrell@cs.tcd.ie>
2025-09-04 19:26:54 +0000

    echstatlogwork

--- scripts/echstat.php
+++ scripts/echstat.php
@@ -12,6 +12,55 @@
 
     $headers = getRequestHeaders();
 
+    /*
+     * Log: time, HMAC(client IP), URL, ech-status, user-agent
+     * We'll store a keyed-hash of the client IP. We could
+     * till locally find the IP from other logs if we wanted,
+     * but this'll mean we can just publish this log.
+     */
+    function do_hmac($plain) {
+        $keyfile="/var/extra/client-ip.key";
+        $key=file_get_contents($keyfile);
+        if ($key===FALSE) {
+            return "no-key";
+        }
+        $hmac = hash_hmac('sha256', $plain, $key, $as_binary=false);
+        return $hmac;
+    }
+    /* we don't expect to be v. busy but just in case */
+    function addone($logline) {
+        $filename="/var/extra/echstat.csv";
+        $gotlock=false;
+        $attempts=0;
+        while ($attempts<5 && !$gotlock) {
+            $fp=fopen($filename,"a");
+            if ($fp!==false && flock($fp, LOCK_EX | LOCK_NB)) {
+                $gotlock=true;
+                $rv=fwrite($fp,$logline . "\n");
+                fflush($fp);
+                flock($fp, LOCK_UN); // unlock the file
+                fclose($fp);
+                if ($rv===FALSE) $gotlock=false;
+            } else {
+                // flock() returned false, no lock obtained
+                print "Could not lock $filename!\n";
+                $attempts++;
+                if ($fp!==false) fclose($fp);
+                if ($attempts<5) sleep(1);
+            }
+        }
+        return $gotlock;
+    }
+    $logstring = "";
+    $logstring = $logstring . "|" . date(DATE_RFC3339);
+    $logstring = $logstring . "|" . do_hmac($_SERVER['REMOTE_ADDR']);
+    $logstring = $logstring . "|" . "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    $logstring = $logstring . "|" . $_SERVER["SSL_ECH_STATUS"];
+    $logstring = $logstring . "|" . $_SERVER["SSL_ECH_INNER_SNI"];
+    $logstring = $logstring . "|" . $_SERVER["SSL_ECH_OUTER_SNI"];
+    $logstring = $logstring . "|" . $_SERVER["HTTP_USER_AGENT"];
+    addone($logstring);
+
     if ($_GET['format'] == "json") {
         header('Content-Type: application/json; charset=utf-8');
         echo "{";
@@ -43,58 +92,6 @@
         echo "config: ". $_SERVER['SERVER_NAME'] . "\n";
 
     }
-
-    /*
-     * Log: time, HMAC(client IP), URL, ech-status, user-agent
-     * We'll store a keyed-hash of the client IP. We could
-     * till locally find the IP from other logs if we wanted,
-     * but this'll mean we can just publish this log.
-     */
-
-    function do_hmac($plain) {
-        $keyfile="/var/extra/client-ip.key";
-        $key=file_get_contents($keyfile);
-        if ($key===FALSE) {
-            return "no-key";
-        }
-        $hmac = hash_hmac('sha256', $plain, $key, $as_binary=false);
-        return $hmac;
-    }
-
-    /* we don't expect to be v. busy but just in case */
-    function addone($logline) {
-        $filename="/var/extra/echstat.csv";
-        $gotlock=false;
-        $attempts=0;
-        while ($attempts<5 && !$gotlock) {
-            $fp=fopen($filename,"a");
-            if ($fp!==false && flock($fp, LOCK_EX | LOCK_NB)) {
-                $gotlock=true;
-                $rv=fwrite($fp,$logline . "\n");
-                fflush($fp);
-                flock($fp, LOCK_UN); // unlock the file
-                fclose($fp);
-                if ($rv===FALSE) $gotlock=false;
-            } else {
-                // flock() returned false, no lock obtained
-                print "Could not lock $filename!\n";
-                $attempts++;
-                if ($fp!==false) fclose($fp);
-                if ($attempts<5) sleep(1);
-            }
-        }
-        return $gotlock;
-    }
-
-    $logstring = "";
-    $timestr = $logstring . "|" . date(DATE_RFC3339);
-    $keyedip = $logstring . "|" . do_hmac($_SERVER['HTTP_CLIENT_IP'])
-    $theurl = $logstring . "|" . "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-    $ech_status = $logstring . "|" . $_SERVER["SSL_ECH_STATUS"];
-    $ech_inner = $logstring . "|" . $_SERVER["SSL_ECH_INNER"];
-    $ech_outer = $logstring . "|" . $_SERVER["SSL_ECH_OUTER"};
-    $uastring = $logstring . "|" . $_SERVER["HTTP_USER_AGENT"];
-    addone($logstring);
 
 ?>
 
